8e0faa6671cb7195d11de171692e3265408e56ce
wenbinteng <wenbinteng@163.com>
2024-11-19 15:30:40 +0800

    update app lenet

---
+++ app/lenet/host/main.c
@@ -0,0 +1,13 @@
+#include "top.h"
+
+#include "xil_printf.h"
+#include "xiltimer.h"
+#include "xparameters.h"
+
+#include "stdlib.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
