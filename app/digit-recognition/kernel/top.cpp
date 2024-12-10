f94d6d2f84b6a2dab619c5d22955cfa8986fcb87
wenbinteng <wenbinteng@163.com>
2024-12-10 15:54:29 +0800

    update app digit-recognition

--- app/digit-recognition/kernel/top.cpp
+++ app/digit-recognition/kernel/top.cpp
@@ -3,5 +3,11 @@
 int digit_rec_kernel_0(WholeDigitType global_training_set[NUM_TRAINING],
                        WholeDigitType global_test_set[NUM_TEST],
                        LabelType global_results[NUM_TEST]) {
+
+#pragma HLS INTERFACE s_axilite register port=return
+#pragma HLS INTERFACE m_axi depth=NUM_TRAINING port=global_training_set offset=slave bundle=data
+#pragma HLS INTERFACE m_axi depth=NUM_TEST port=global_test_set offset=slave bundle=data
+#pragma HLS INTERFACE m_axi depth=NUM_TEST port=global_results offset=slave bundle=data
+
     digit_rec(global_training_set, global_test_set, global_results);
 }
