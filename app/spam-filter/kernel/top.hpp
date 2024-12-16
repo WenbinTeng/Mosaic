cde3be206f05d163b44c657b108b158a59c72295
wenbinteng <wenbinteng@163.com>
2024-12-16 16:15:24 +0800

    update app spam-filter

--- app/spam-filter/kernel/top.hpp
+++ app/spam-filter/kernel/top.hpp
@@ -3,8 +3,8 @@
 
 #include "sgd.hpp"
 
-void spam_fil_kernel_0(VectorDataType data[NUM_FEATURES * NUM_TRAINING / D_VECTOR_SIZE],
-                       VectorLabelType label[NUM_TRAINING / L_VECTOR_SIZE],
-                       VectorFeatureType theta[NUM_FEATURES / F_VECTOR_SIZE]);
+int spam_fil_kernel_0(VectorDataType data[NUM_FEATURES * NUM_TRAINING / D_VECTOR_SIZE],
+                      VectorLabelType label[NUM_TRAINING / L_VECTOR_SIZE],
+                      VectorFeatureType theta[NUM_FEATURES / F_VECTOR_SIZE]);
 
 #endif
